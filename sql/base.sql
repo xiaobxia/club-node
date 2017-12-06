a357cac7ffc8ff53d25e53e31e30ed9286ee6f41
chenlingjie <chenlingjie@cd121.com>
2017-12-06 10:56:55 +0800

    xg

--- sql/base.sql
+++ sql/base.sql
@@ -4,6 +4,7 @@ DROP TABLE IF EXISTS sys_user;
 
 DROP TABLE IF EXISTS sys_email_verify;
 
+DROP TABLE IF EXISTS broadcast;
 /*==============================================================*/
 /* Table: sys_log_audit                                         */
 /*==============================================================*/
@@ -26,8 +27,8 @@ CREATE TABLE sys_user
    user_id              INT(11) NOT NULL AUTO_INCREMENT,
    user_name            VARCHAR(60) NOT NULL COMMENT '登陆账户名',
    password             VARCHAR(60) NOT NULL COMMENT '登陆账户密码',
-   mobile               VARCHAR(20),
-   email                VARCHAR(50),
+   mobile               VARCHAR(20) NOT NULL DEFAULT '',
+   email                VARCHAR(50) NOT NULL DEFAULT '',
    active               CHAR(1) NOT NULL DEFAULT 'N' COMMENT '是否已激活账户，Y已激活，N未激活',
    active_date          DATETIME COMMENT '激活的日期',
    state                CHAR(1) NOT NULL DEFAULT 'A' COMMENT 'A-在用，X-失效',
@@ -55,5 +56,39 @@ CREATE TABLE sys_email_verify
    verify_status        INT(1) NOT NULL COMMENT '验证状态，1：邮件已发送，2：邮件验证成功，2：邮件验证失败',
    update_date          DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
    create_date          DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   PRIMARY KEY (id)
+)
+
+/*==============================================================*/
+/* Table: broadcast(广播)                                          */
+/*==============================================================*/
+CREATE TABLE broadcast
+(
+   id                   BIGINT(9) NOT NULL AUTO_INCREMENT,
+   uuid                 CHAR(32) NOT NULL COMMENT '广播的uuid',
+   create_date          DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   update_date          DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   starte_date          DATETIME COMMENT '广播上线时间',
+   end_date             DATETIME COMMENT '广播下线时间',
+   state                CHAR(1) NOT NULL DEFAULT 'A' COMMENT 'A-在用，X-失效',
+   platform             CHAR(10) NOT NULL DEFAULT 'web' COMMENT '广播上线平台',
+   title                VARCHAR(150) NOT NULL DEFAULT '' COMMENT '广播的标题',
+   url                  VARCHAR(255) NOT NULL DEFAULT '' COMMENT '广播的地址',
+   PRIMARY KEY (id)
+)
+
+/*==============================================================*/
+/* Table: system_message(系统消息)                               */
+/*==============================================================*/
+CREATE TABLE system_message
+(
+   id                   BIGINT(9) NOT NULL AUTO_INCREMENT,
+   uuid                 CHAR(32) NOT NULL COMMENT '系统消息的uuid',
+   create_date          DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   update_date          DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   start_date           DATETIME COMMENT '系统消息发送时间',
+   state                CHAR(1) NOT NULL DEFAULT 'A' COMMENT 'A-在用，X-失效',
+   title                VARCHAR(150) NOT NULL DEFAULT '' COMMENT '系统消息的标题',
+   url                  VARCHAR(255) NOT NULL DEFAULT '' COMMENT '系统消息的地址',
    PRIMARY KEY (id)
 )
